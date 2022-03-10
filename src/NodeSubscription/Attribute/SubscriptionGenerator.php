dae8cbf892a8b4e11989e3a58a6f3a348b074d7b
Mateusz Mesek <mateusz@mesek.pl>
2022-03-10 20:37:04 +0100

    wip

---
+++ src/NodeSubscription/Attribute/SubscriptionGenerator.php
@@ -0,0 +1,72 @@
+<?php declare(strict_types=1);
+
+namespace MateuszMesek\DocumentDataCatalogCategoryMview\NodeSubscription\Attribute;
+
+use InvalidArgumentException;
+use Magento\Catalog\Api\Data\CategoryInterface;
+use Magento\Eav\Model\Config;
+use Magento\Framework\DB\Ddl\Trigger;
+use Magento\Framework\EntityManager\MetadataPool;
+use MateuszMesek\DocumentDataIndexerMview\Data\SubscriptionFactory;
+use Traversable;
+
+class SubscriptionGenerator
+{
+    private MetadataPool $metadataPool;
+    private Config $config;
+    private SubscriptionFactory $subscriptionFactory;
+
+    public function __construct(
+        MetadataPool $metadataPool,
+        Config $config,
+        SubscriptionFactory $subscriptionFactory
+    )
+    {
+        $this->metadataPool = $metadataPool;
+        $this->config = $config;
+        $this->subscriptionFactory = $subscriptionFactory;
+    }
+
+    public function generate(string $code): Traversable
+    {
+        $metadata = $this->metadataPool->getMetadata(CategoryInterface::class);
+
+        $attribute = $this->config->getAttribute($metadata->getEavEntityType(), $code);
+
+        if (!$attribute) {
+            throw new InvalidArgumentException("Attribute '$code' not found");
+        }
+
+        foreach (Trigger::getListOfEvents() as $event) {
+            switch ($event) {
+                case Trigger::EVENT_INSERT:
+                case Trigger::EVENT_UPDATE:
+                    $prefix = 'NEW';
+                    break;
+
+                case Trigger::EVENT_DELETE:
+                    $prefix = 'OLD';
+                    break;
+
+                default:
+                    throw new InvalidArgumentException("Trigger event '$event' is unsupported");
+            }
+
+            $condition = '';
+            $dimensions = "JSON_SET('{}', '$.scope', 0)";
+
+            if (!$attribute->isStatic()) {
+                $condition = "$prefix.attribute_id = {$attribute->getAttributeId()}";
+                $dimensions = "JSON_SET('{}', '$.scope', $prefix.store_id)";
+            }
+
+            yield $this->subscriptionFactory->create([
+                'tableName' => $attribute->getBackendTable(),
+                'triggerEvent' => $event,
+                'condition' => $condition,
+                'documentId' => "(SELECT {$metadata->getIdentifierField()} FROM {$metadata->getEntityTable()} WHERE {$metadata->getLinkField()} = $prefix.{$metadata->getLinkField()})",
+                'dimensions' => $dimensions,
+            ]);
+        }
+    }
+}
